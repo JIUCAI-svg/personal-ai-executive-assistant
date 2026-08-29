6173cb3e98f8b95c10f67bf1514b2b4fe6f01774
JIUCAI <2382821355zmy@gmail.com>
2026-08-29 12:57:19 +0800

    chore(android): bump tool calling build to 0.5.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.forward.assistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.1"
+        versionCode = 6
+        versionName = "0.5.2"
     }
 
     buildTypes {
