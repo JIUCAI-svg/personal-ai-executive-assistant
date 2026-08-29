b349a21c08d773531caf13b20aebdc313255303d
JIUCAI <2382821355zmy@gmail.com>
2026-08-29 19:18:39 +0800

    chore(android): bump click interaction build to 0.5.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.forward.assistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.5.7"
+        versionCode = 9
+        versionName = "0.5.8"
     }
 
     buildTypes {
